9b6255819e85465cf6d0fe08fc0b15c1fcd3b930
himanshu gautam <himanshu0000gautam@gmail.com>
2025-05-29 12:53:58 +0530

    fisrt stage

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ window.onload = function() {
       loop: true
     });
   };
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwT8tUgTrB0ZtDn9cbEgyeTTtziv40-zk_iLwOGMD4DdlIZQ2xLNHObacVPxtX01kc/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+
